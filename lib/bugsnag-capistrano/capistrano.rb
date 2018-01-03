5e1e7572ea20dd64b10eae0a45c691a332c0179c
Alex Moinet <alex@bugsnag.com>
2018-01-03 17:14:04 +0000

    Fix invalid return error on older rubies

--- lib/bugsnag-capistrano/capistrano.rb
+++ lib/bugsnag-capistrano/capistrano.rb
@@ -1,9 +1,7 @@
 require_relative "deploy"
 
-if !defined?(Capistrano)
-  return
-elsif defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
+if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
   load File.expand_path('../tasks/bugsnag.cap', __FILE__)
-else
+elsif defined?(Capistrano)
   require_relative 'capistrano2'
 end
